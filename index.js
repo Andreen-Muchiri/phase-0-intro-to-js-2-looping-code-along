becfc27203b2ce0ec7611a2c4fbbcdf36a9e6b50
Andreen <andreenshii@gmail.com>
2022-08-12 19:56:50 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards () {
+  let message = []
+  const names =['Guadalupe','Ollie','Aki'];
+  for (let i = 0; i < names.length; i++) {
+    console.log (message.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`));
+    debugger;
+
+}
+return message;
+}
+writeCards(names);
+
+function countDown() {
+  let i = 0;
+  while( i < 11) {
+    console.log(i ++);
+    debugger;
+  }
+}
